0e6cc391c0c6d6b95eb3d9b2e79a08e9179c4023
eupho <10086@qq.com>
2017-09-02 19:57:30 +0800

    add log function

--- uml.sh
+++ uml.sh
@@ -1,43 +1,86 @@
-log() {
+## log file
+function log() {
 	msg = $1
-	echo $msg > log.file
+	echo $msg >> log.txt
 
 }
 
+## green echo
+function green(){
+	echo -e "\033[32m\033[01m\033[05m[ $1 ]\033[0m"
+	log $1
+}
+
+## error
+function check() {
+	if [ $? -eq 0]
+	then
+		log "$@ sucessed."
+	else
+		log "$@ failed."
+		end_date = `date "+%Y-%m-%d %H:%M:%S"`
+		log "end at : ${end_date}"
+		log "==========END=========="
+		exit 1
+}
 
 #download some required files
-echo "PREPARE ENVIRONMENT..."
+log "==========START=========="
+
+date = `date "+%Y-%m-%d %H:%M:%S"`
+user = `whoami`
+log "${date} ${user} execute ..."
+
+green "PREPARE ENVIRONMENT..."
 apt-get update && apt-get install build-essential libtool automake libncurses5-dev kernel-package
-echo "DOWNLOAD KERNEL SOURCE CODE..."
+green "PREPARE DONE"
+
+green "DOWNLOAD KERNEL SOURCE CODE..."
 mkdir uml && cd uml
 wget http://mirrors.ustc.edu.cn/kernel.org/linux/kernel/v4.x/testing/linux-4.9-rc5.tar.gz -O kernel.tar.gz && tar -xzf kernel.tar.gz
 cd linux-4.9-rc5
 
 #build kernel
-echo "START BUILDING KERNEL SOURCE CODE..."
+green "START BUILDING KERNEL SOURCE CODE..."
+
+green "MAKE DEFAULT CONFIG.."
 make defconfig ARCH=um
-echo "MAKE DEFAULT CONFIG.."
-echo "BUILDING START.."
+
+green "BUILDING START.."
 make vmlinux ARCH=um
+
 cp vmlinux ..
 cd ..
-echo "BUILDING FINISHED.."
+green "BUILDING FINISHED.."
 
 #make rootfs
-echo "INSTALL DEBOOTSTRAP.."
+green "INSTALL DEBOOTSTRAP.."
 apt-get install debootstrap
-echo "INSTALL FINISH"
-echo "START BUILDING ROOT FILESYSTEM.."
+green "INSTALL FINISH"
+
+green "START BUILDING ROOT FILESYSTEM.."
 fallocate -l 4G rootfs && mkfs.ext4 rootfs
 mkdir mnt && mount rootfs mnt
-debootstrap --arch amd64 xenial mnt/
-echo "please use the command 'passwd' to set your root password theninput 'exit' to quit"
-#gnome-terminal -t "set root password" -x bash -c "chroot mnt"
+debootstrap --arch amd64 xenial mnt/ >> log.txt
+check "bootstrap"
+
+green "please use the command 'passwd' to set your root password theninput 'exit' to quit"
+chroot mnt
+
+wait 1
 echo "/dev/ubda / ext4 defaults 0 0" > mnt/etc/fstab
+check "make fstab file"
+
 wait 3
+green "OMOUNTING..."
 umount mnt
+check "umount"
 
 #run uml
 apt-get install uml-utilities screen
+green "SET UP UML"
 vmlinux ubda=rootfs mem=256M con=pts con1=fd:0,fd:1
-
+check "set up"
+end_date = `date "+%Y-%m-%d %H:%M:%S"`
+log "end at : ${end_date}"
+log "==========END=========="
