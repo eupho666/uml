e227145c0b326ee6050a2096c74c487ce8f959c7
eupho <10086@qq.com>
2017-09-06 12:23:10 +0800

    success run..

--- uml.sh
+++ uml.sh
@@ -4,7 +4,7 @@
 
 ## green echo and log
 green(){
-	echo -e "\033[32m\033[01m\033[05m[ $1 ]\033[0m"
+	echo -e "\033[32m\033[01m\033[05m$1\033[0m"
 	name=`pwd`
 	if [ ${name:(-3):3} = "hhh" ]
 	then
@@ -19,20 +19,20 @@ green(){
 
 ## error
 check() {
-	if [ $? -eq 0]
+	if [ $? -eq 0 ]
 	then
-		log "$@ sucessed."
+		green "$@ sucessed."
 	else
-		log "$@ failed."
-		end_date = `date "+%Y-%m-%d %H:%M:%S"`
-		log "end at : ${end_date}"
-		log "==========END=========="
+		green "$@ failed."
+		end_date=`date "+%Y-%m-%d %H:%M:%S"`
+		green "end at : ${end_date}"
+		green "==========END=========="
 		exit 1
 	fi
 }
 
 # check permission
-if [ $EUID -nq 0 ]
+if [ $EUID != 0 ]
 then
 	echo "please run as root permission"
 	exit 1
@@ -47,46 +47,46 @@ fi
 # start
 green "==========START=========="
 
-date =`date "+%Y-%m-%d %H:%M:%S"`
-user =`whoami`
+date=`date "+%Y-%m-%d %H:%M:%S"`
+user=`whoami`
 green "${date} ${user} execute ..."
 
 #download some required files
-green "PREPARE ENVIRONMENT..."
-apt-get update && apt-get install build-essential libtool automake libncurses5-dev kernel-package
-green "PREPARE DONE"
+green "[*]PREPARE ENVIRONMENT..."
+apt-get update && apt-get install build-essential libtool automake libncurses5-dev kernel-package | tee -a log.txt
+green "[*]PREPARE DONE"
 
-green "DOWNLOAD KERNEL SOURCE CODE..."
+green "[*]DOWNLOAD KERNEL SOURCE CODE..."
 mkdir uml-hhh && cd uml-hhh
 wget http://mirrors.ustc.edu.cn/kernel.org/linux/kernel/v4.x/testing/linux-4.9-rc5.tar.gz -O kernel.tar.gz
 tar -xzvf kernel.tar.gz
 cd linux-4.9-rc5
 
 #build kernel
-green "START BUILDING KERNEL SOURCE CODE..."
+green "[*]START BUILDING KERNEL SOURCE CODE..."
 
-green "MAKE DEFAULT CONFIG.."
+green "[*]MAKE DEFAULT CONFIG.."
 make defconfig ARCH=um
 
-green "BUILDING START.."
-make vmlinux ARCH=um
+green "[*]BUILDING START.."
+make vmlinux ARCH=um | tee -a ../../log.txt
 
 cp vmlinux ..
 cd ..
-green "BUILDING FINISHED.."
+green "[*]BUILDING FINISHED.."
 
 #make rootfs
-green "INSTALL DEBOOTSTRAP.."
-apt-get install debootstrap
-green "INSTALL FINISH"
+green "[*]INSTALL DEBOOTSTRAP.."
+apt-get install debootstrap | tee -a ../log.txt
+green "[*]INSTALL FINISH"
 
-green "START BUILDING ROOT FILESYSTEM.."
+green "[*]START BUILDING ROOT FILESYSTEM.."
 fallocate -l 4G rootfs && mkfs.ext4 rootfs
 mkdir mnt && mount rootfs mnt
-debootstrap --arch amd64 xenial mnt/ >> log.txt
+debootstrap --arch amd64 xenial mnt/ | tee -a ../log.txt
 check "bootstrap"
 
-green "please use the command 'passwd' to set your root password theninput 'exit' to quit"
+green "[*]please use the command 'passwd' to set your root password theninput 'exit' to quit"
 chroot mnt
 
 wait 1
@@ -94,16 +94,16 @@ echo "/dev/ubda / ext4 defaults 0 0" > mnt/etc/fstab
 check "make fstab file"
 
 wait 3
-green "OMOUNTING..."
+green "[*]UMOUNTING..."
 umount mnt
-check "umount"
+check "[*]umount"
 
 #run uml
 apt-get install uml-utilities screen
-green "SET UP UML"
-vmlinux ubda=rootfs mem=256M con=pts con1=fd:0,fd:1
+green "[*]SET UP UML"
+./vmlinux ubda=rootfs mem=256M con=pts con1=fd:0,fd:1 | tee -a ../log.txt
 check "set up"
 rm -rf kernel.tar.gz
-end_date = `date "+%Y-%m-%d %H:%M:%S"`
+end_date=`date "+%Y-%m-%d %H:%M:%S"`
 green "end at : ${end_date}"
 green "==========END=========="
